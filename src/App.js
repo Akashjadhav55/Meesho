fcb86ccd71834373bfa6ba49531f2c576b8d1bcd
Akash Jadhav <akashjadhav7040253178@gmail.com>
2022-10-12 22:49:28 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Route from './Routes/Route';
+import RouteElement from './Routes/Route';
 
 function App() {
   return (
     <div className="App">
-
+      <RouteElement />
     </div>
   );
 }
