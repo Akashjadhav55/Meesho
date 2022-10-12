c5f6efca90625d9caa8d6012ac60a5ecfbb5a46d
Akash Jadhav <akashjadhav7040253178@gmail.com>
2022-10-12 22:56:14 +0530

    push

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom"
+import Home from '../components/Home/Home'
 
 export default function RouteElement() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={ <Home /> } ></Route>
+        {/* <Route path='/' element={} ></Route>
+        <Route path='/' element={} ></Route>
+        <Route path='/' element={} ></Route> */}
+      </Routes>
     </div>
   )
 }
