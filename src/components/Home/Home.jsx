4dd14b6fc28f5f781a2590b7b18d9a522929e5e4
Akash Jadhav <akashjadhav7040253178@gmail.com>
2022-10-18 21:14:41 +0530

    add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
+import React from "react";
+import styles from "./Home.module.css"
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.containerAds}>
         <div>
+          <h1>Lowest Prices Best Qaulity Shopping</h1>
           <div>
-            <h1>Lowest Prices Best Qaulity Shopping</h1>
             <div>
-              <div>
-                <img src="" alt="" />
-                <p>Free Delivery</p>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <p>Cash On Delivery</p>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <p>Easy Returns</p>
-              </div>
+              <img src="" alt="" />
+              <p>Free Delivery</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Cash On Delivery</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Easy Returns</p>
             </div>
           </div>
-          <div>
-
-          </div>
         </div>
-
-    
+        <div>
+          <img src="https://images.meesho.com/images/marketing/1631722939962.webp" alt="" />
+        </div>
     </div>
-  )
+  );
 }
