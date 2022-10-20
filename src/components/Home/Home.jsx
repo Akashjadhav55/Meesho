69dbbd55904177da2d9c54a98d7dc97c352358e6
Akash Jadhav <akashjadhav7040253178@gmail.com>
2022-10-20 19:01:27 +0530

    add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import logo from "../../Img/playstore.webp"
 
 export default function Home() {
   return (
+    <div>
     <div className={styles.containerAds}>
         <div className={styles.containerTwo}>
           <h1>Lowest Prices Best Qaulity Shopping</h1>
@@ -14,10 +15,12 @@ export default function Home() {
               <img src="https://images.meesho.com/images/pow/freeDelivery.svg" alt="" />
               <p>Free <br /> Delivery</p>
             </div>
+            <div className={styles.blank}></div>
             <div>
               <img src="https://images.meesho.com/images/pow/cod.svg" alt="" />
               <p>Cash On <br /> Delivery</p>
             </div>
+            <div className={styles.blank}></div>
             <div>
               <img src="https://images.meesho.com/images/pow/easyReturns.svg" alt="" />
               <p>Easy <br /> Returns</p>
@@ -29,5 +32,17 @@ export default function Home() {
           <img className={styles.AdsImage} src="https://images.meesho.com/images/marketing/1631722939962.webp" alt="" />
         </div>
     </div>
+    <p className={styles.Heading} >Top Categories to choose from</p>
+
+    <div className={styles.AdsImage2} >
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+    </div>
+
   );
 }
