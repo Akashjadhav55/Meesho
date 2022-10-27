7ce4fdb905c053499a2ce48fda226c8374fd3c00
Akash Jadhav <akashjadhav7040253178@gmail.com>
2022-10-28 01:50:43 +0530

    push

--- src/components/Home/Product.jsx
+++ src/components/Home/Product.jsx
@@ -1,6 +1,6 @@
-import { ImportContactsRounded } from '@mui/icons-material'
 import axios from 'axios'
 import React from 'react'
+import styles from "./Home.module.css"
 
 function Product() {
   const [data , setdata] = React.useState([])
@@ -23,7 +23,20 @@ function Product() {
 
   return (
     <div>
-      
+      {
+        data.map((item) =>(
+          <div style={{border : "1px solid" , display : "grid", gridTemplate : "repeat(2, 1fr)"}}>
+            <img className={styles.img} src={item.image} alt="" />
+            <h3>{item.title}</h3>
+            <h2>{item.price}</h2>
+            <p>Free Delivery</p>
+            <div className={styles.rating}>
+              <p>{item.rating.rate}</p>
+              <p>{item.rating.count}</p>
+            </div>
+          </div>
+        ))
+      }
         
     </div>
   )
