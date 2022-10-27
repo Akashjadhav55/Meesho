f666a10a540fc2a2f5fdf24a6210c57a58285a18
Akash Jadhav <akashjadhav7040253178@gmail.com>
2022-10-28 01:27:33 +0530

    push

--- src/components/Home/Product.jsx
+++ src/components/Home/Product.jsx
@@ -1,7 +1,25 @@
+import { ImportContactsRounded } from '@mui/icons-material'
+import axios from 'axios'
 import React from 'react'
 
 function Product() {
-    
+  const [data , setdata] = React.useState([])
+
+  React.useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    axios.get("https://fakestoreapi.com/products", {
+      responseType : "json",
+    })
+    .then(function(response){
+      setdata(response.data)
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+  }
 
   return (
     <div>
